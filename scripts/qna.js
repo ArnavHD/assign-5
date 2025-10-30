d45a20394a0ff407d151d73e92d70701e460cc72
ArnavHD <arnavpaul180@gmail.com>
2025-10-30 09:04:05 -0600

    Done with all the functionality

--- scripts/qna.js
+++ scripts/qna.js
@@ -1,5 +1,5 @@
 // back to previous page
 document.getElementById('back-to')
     .addEventListener('click', function () {
-        console.log('back to back click');
+        window.location.href = './index.html';
     })
